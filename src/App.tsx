ccee93fbd8cf20d6b752216cdef2a915544a1a54
Vincent Solaberrieta <vincent.solaberrieta@pytheascapital.com>
2023-11-17 16:29:38 +0100

    wip: wip nor nork orainaldia

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Orainaldia from "./ariketak/nor-nork/Orainaldia";
 
 function App() {
   return (
     <div className="App">
-      <p>Bombardaketak</p>
+      <Orainaldia />
     </div>
   );
 }
